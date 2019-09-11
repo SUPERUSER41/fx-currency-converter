bc07a6afec41e975505511a64938d589052369d3
Daneil Greaves <daneil@itspixelperfect.com>
2019-09-11 11:51:05 -0500

    Update Switch.js
    Change path to Switch.css

--- app/currency-converter-components/Switch.js
+++ app/currency-converter-components/Switch.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import "../assets/css/Switch.css";
+import "../../assets/css/Switch.css";
 import Switch from "react-switch";
 import { connect } from "react-redux";
 
